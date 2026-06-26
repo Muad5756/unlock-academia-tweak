5ae51244b7f7b2aa5da6fb18cc26145ba14d15b6
Muad Hisham <muadhishamsub4@oxaam.com>
2026-06-26 14:28:24 +0200

    add RevenueCat bypass hooks (isActive, RCEntitlementInfos.all, PurchasesFlutterPlugin handleMethodCall)

--- unlock_academia.m
+++ unlock_academia.m
@@ -96,6 +96,121 @@ static void uitextfield_setSecureTextEntry(id self, SEL _cmd, BOOL val) {
 
 static void preventer_enablePreventScreenshot(id self, SEL _cmd) {}
 
+#pragma mark - RevenueCat Bypass
+
+static BOOL rc_entitlementInfo_isActive(id self, SEL _cmd) { return YES; }
+static BOOL rc_entitlementInfo_isActiveInCurrentEnvironment(id self, SEL _cmd) { return YES; }
+
+static id (*orig_rc_entitlementInfos_all)(id, SEL);
+static id rc_entitlementInfos_all(id self, SEL _cmd) {
+    id result = orig_rc_entitlementInfos_all ? orig_rc_entitlementInfos_all(self, _cmd) : nil;
+    if (!result) result = @{};
+    if (!result[@"RC_BILLING"]) {
+        NSMutableDictionary *dict = [result mutableCopy];
+        Class infoClass = NSClassFromString(@"RCEntitlementInfo");
+        if (infoClass) {
+            id info = ((id (*)(id, SEL))objc_msgSend)(infoClass, @selector(alloc));
+            if (info) {
+                info = ((id (*)(id, SEL, SEL))objc_msgSend)(info, @selector(init));
+                if (info) dict[@"RC_BILLING"] = info;
+            }
+        }
+        result = [dict copy];
+    }
+    return result;
+}
+
+static void (*orig_rc_handleCall)(id, SEL, id, id);
+
+// Helper to inject RC_BILLING into customer info dicts
+static id ensureRCBilling(id data) {
+    if (![data isKindOfClass:NSDictionary.class]) return data;
+    NSMutableDictionary *mdict = [(NSDictionary *)data mutableCopy];
+    id ents = mdict[@"entitlements"];
+    NSMutableDictionary *ment = [ents mutableCopy] ?: [NSMutableDictionary dictionary];
+    if (!ment[@"RC_BILLING"]) {
+        ment[@"RC_BILLING"] = @{
+            @"identifier": @"RC_BILLING",
+            @"isActive": @YES,
+            @"willRenew": @YES,
+            @"periodType": @"NORMAL",
+            @"latestPurchaseDate": @"2024-01-01T00:00:00Z",
+            @"originalPurchaseDate": @"2024-01-01T00:00:00Z",
+            @"expirationDate": @"2099-12-31T23:59:59Z",
+            @"store": @"app_store",
+            @"productIdentifier": @"com.speetar.academia.monthly",
+            @"productPlanIdentifier": NSNull.null,
+            @"purchaseDate": @"2024-01-01T00:00:00Z",
+            @"ownershipType": @"PURCHASED",
+        };
+    }
+    mdict[@"entitlements"] = ment;
+    mdict[@"allExpirationDates"] = @{@"RC_BILLING": @"2099-12-31T23:59:59Z"};
+    mdict[@"allPurchaseDates"] = @{@"RC_BILLING": @"2024-01-01T00:00:00Z"};
+    return [mdict copy];
+}
+
+static void rc_plugin_handleCall(id self, SEL _cmd, id call, id result) {
+    NSString *method = ((NSString *(*)(id, SEL))objc_msgSend)(call, @selector(method));
+
+    if ([method isEqualToString:@"getCustomerInfo"] || [method isEqualToString:@"getOfferings"]) {
+        void (^origResult)(id) = result;
+        void (^wrapped)(id) = ^(id data) {
+            origResult(ensureRCBilling(data));
+        };
+        if (orig_rc_handleCall) {
+            orig_rc_handleCall(self, _cmd, call, (id)wrapped);
+        } else {
+            wrapped(nil);
+        }
+        return;
+    }
+
+    if (orig_rc_handleCall) orig_rc_handleCall(self, _cmd, call, result);
+}
+
+static void applyRevenueCatHooks() {
+    Class infoClass = NSClassFromString(@"RCEntitlementInfo");
+    if (infoClass) {
+        swzClassIfSafe(infoClass, @selector(isActive), (IMP)rc_entitlementInfo_isActive);
+        swzClassIfSafe(infoClass, @selector(isActiveInCurrentEnvironment), (IMP)rc_entitlementInfo_isActiveInCurrentEnvironment);
+        NSLog(@"[unlock_academia] RCEntitlementInfo.isActive hooked");
+    }
+
+    Class infosClass = NSClassFromString(@"RCEntitlementInfos");
+    if (infosClass) {
+        Method allM = class_getInstanceMethod(infosClass, @selector(all));
+        if (allM) {
+            orig_rc_entitlementInfos_all = (id (*)(id, SEL))method_getImplementation(allM);
+            method_setImplementation(allM, (IMP)rc_entitlementInfos_all);
+        }
+        NSLog(@"[unlock_academia] RCEntitlementInfos.all hooked");
+    }
+
+    Class pluginClass = NSClassFromString(@"PurchasesFlutterPlugin");
+    if (pluginClass) {
+        Method hm = class_getInstanceMethod(pluginClass, @selector(handleMethodCall:result:));
+        if (hm) {
+            orig_rc_handleCall = (void (*)(id, SEL, id, id))method_getImplementation(hm);
+            method_setImplementation(hm, (IMP)rc_plugin_handleCall);
+        }
+        NSLog(@"[unlock_academia] PurchasesFlutterPlugin.handleMethodCall:result: hooked");
+    }
+
+    // Also hook via Flutter plugin registrar if available
+    Class pluginRegistrar = NSClassFromString(@"PurchasesPlugin");
+    if (!pluginRegistrar) pluginRegistrar = NSClassFromString(@"RCPurchasesPlugin");
+    if (pluginRegistrar) {
+        Method hm2 = class_getInstanceMethod(pluginRegistrar, @selector(handleMethodCall:result:));
+        if (!hm2) hm2 = class_getInstanceMethod(pluginRegistrar, @selector(handle:result:));
+        if (hm2 && !orig_rc_handleCall) {
+            orig_rc_handleCall = (void (*)(id, SEL, id, id))method_getImplementation(hm2);
+            method_setImplementation(hm2, (IMP)rc_plugin_handleCall);
+            NSLog(@"[unlock_academia] %@ handle hooked", pluginRegistrar);
+        }
+    }
+}
+
 #pragma mark - Hooking Logic
 
 static void applyUIKitHooks() {
@@ -230,6 +345,7 @@ static void runDeferredHooks() {
 
     applySafeDeviceHooks();
     applyScreenPreventerHooks();
+    applyRevenueCatHooks();
 
     // Force-disable any secure text fields in the window hierarchy
     scanAndDisableSecureTextFields();
@@ -239,6 +355,7 @@ static void runDeferredHooks() {
                    dispatch_get_main_queue(), ^{
         applySafeDeviceHooks();
         applyScreenPreventerHooks();
+        applyRevenueCatHooks();
         scanAndDisableSecureTextFields();
         NSLog(@"[unlock_academia] Late retry complete");
     });
